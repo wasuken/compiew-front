93f9a144b05b2f89ba4b9b508a3adf6ba5621589
wasuken <wevorence@gmail.com>
2022-06-24 09:34:01 +0900

    tar.gz,zipファイルのパース機能及びinput type=file経由でのTree作成も完了

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,25 @@ import FolderTree from "react-folder-tree";
 import logo from "./logo.svg";
 import "./App.css";
 import * as pako from "pako";
+import untar from "js-untar";
+import { Zlib as Unzip } from "zlibjs/bin/unzip.min";
 import { isUint8Array } from "util/types";
+
+function parseGZip(
+  a: ArrayBuffer,
+  setFilepathes: (v: string[]) => void
+): string[] {
+  untar(pako.inflate(a).buffer).then((files) =>
+    setFilepathes(files.map((f) => f.name))
+  );
+}
+function parseZip(
+  a: ArrayBuffer,
+  setFilepathes: (v: string[]) => void
+): string[] {
+  let zip = new Unzip.Unzip(new Uint8Array(a));
+  setFilepathes(zip.getFilenames());
+}
 
 const useHooks = () => {
   const [filepathes, setFilepathes] = useState<string[]>([]);
@@ -12,18 +30,28 @@ const useHooks = () => {
     const files = e.currentTarget.files;
     if (!files || files?.length <= 0) return;
 
-    reader.readAsArrayBuffer(files[0]);
+    const file = files[0];
+    reader.readAsArrayBuffer(file);
+    const ext = file.name.split(".").slice(-1)[0];
+    const f = ext === "zip" ? parseZip : parseGZip;
     reader.onload = () => {
-      const inf = pako.inflate(reader.result);
-      const str = new TextDecoder().decode(inf).replaceAll(/\0+/gi, "\0");
-      const fps = str
-        .split("\n\0")
-        .map((x) => x.split("\0")[0])
-        .filter((x) => x.replaceAll(/\0+/gi, "").length >= 1);
-      setFilepathes(fps);
+      f(reader.result, setFilepathes);
     };
   };
-  return { handleFiles, filepathes };
+  const handleParseFromUrl = (url: string) => {
+    const uurl = url.split("?")[0];
+    const ext = uurl.split(".").slice(-1)[0];
+    const f = ext === "zip" ? parseZip : parseGZip;
+    let xhr = new XMLHttpRequest();
+    xhr.open("GET", url, true);
+    xhr.responseType = "arraybuffer";
+    xhr.send();
+
+    xhr.addEventListener("load", function () {
+      f(xhr.response);
+    });
+  };
+  return { handleFiles, filepathes, handleParseFromUrl };
 };
 
 function genTable(fps: string[]) {
@@ -55,6 +83,11 @@ function genTable(fps: string[]) {
   return tbl;
 }
 
+interface Node {
+  name: string;
+  children: Node[];
+}
+
 function genDataRoot(fps: string[]) {
   let tbl = genTable(fps);
   let data = {};
@@ -80,7 +113,8 @@ const genDefaultChild = (name: string) => {
 
 function App() {
   const [uploadFile, setUploadFile] = useState("");
-  const { handleFiles, filepathes } = useHooks();
+  const { handleFiles, filepathes, handleParseFromUrl } = useHooks();
+  const [inputUrl, setInputUrl] = useState<string>("");
 
   return (
     <div>
@@ -88,7 +122,17 @@ function App() {
         <h2 className="title">File Input</h2>
         <div>
           <input type="file" onChange={handleFiles} />
-          <button>read</button>
+        </div>
+      </div>
+      <div>
+        <h2 className="title">URL Input</h2>
+        <div>
+          <input
+            type="url"
+            placeholder="zip or gz file url"
+            onChange={(e) => setInputUrl(e.target.value)}
+          />
+          <button onClick={() => handleParseFromUrl(inputUrl)}>read</button>
         </div>
       </div>
       <div>
